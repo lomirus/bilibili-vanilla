f1032f5c028ef81e3d25a9de58c87c27ba2f7ecd
Rhys <lomirus@qq.com>
2021-02-15 17:10:56 +0800

    feat: 增加了没什么卵用的pwa

---
+++ sw.js
@@ -0,0 +1,13 @@
+console.log('注册成功')
+
+self.addEventListener('install', () => {
+    console.log('安装成功')
+})
+
+self.addEventListener('activate', () => {
+    console.log('激活成功')
+})
+
+self.addEventListener('fetch', event => {
+    console.log(event.request)
+})
