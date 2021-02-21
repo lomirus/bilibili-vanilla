691be1982c54b46234d91d337c7299585b79873a
Rhys <lomirus@qq.com>
2021-02-21 10:50:13 +0800

    feat: 空间页面基础信息展示

--- statics/js/space.js
+++ statics/js/space.js
@@ -1,3 +1,7 @@
+const up_avatar = document.querySelector('#banner>.left>img')
+const up_username = document.querySelector('#banner>.left>.userinfo>.username')
+const up_statement = document.querySelector('#banner>.left>.statement')
+const up_level = document.querySelector('#banner>.left>.userinfo>.level')
 const tab_home = document.querySelector('#tab_home')
 const tab_moments = document.querySelector('#tab_moments')
 const tab_post = document.querySelector('#tab_post')
@@ -24,8 +28,27 @@ function queryToJson() {
     }
     return json
 }
-
+function initUser() {
+    getUserInfo(queries.id).then(json => {
+        if (json.status) {
+            console.log('UP: ', json.data)
+            up_avatar.src = json.data.Avatar
+            up_username.textContent = json.data.Username
+            up_statement.textContent = json.data.Statement
+            up_level.setAttribute('lv', getLevel(json.data.Exp))
+            /*author_avatar.style.backgroundImage = `url(${json.data.Avatar})`
+            author_avatar.href = `/space/?id=${json.data.Uid}`
+            author_username.textContent = json.data.Username
+            author_username.href = `/space/?id=${json.data.Uid}`
+            author_statement.textContent = json.data.Statement*/
+        } else {
+            alert('获取 UP 信息失败：' + json.data)
+        }
+    })
+}
 function init() {
+    initUser()
+
     tab_home.addEventListener('mouseover', () => moveUnderline('15px'))
     tab_moments.addEventListener('mouseover', () => moveUnderline('96px'))
     tab_post.addEventListener('mouseover', () => moveUnderline('177px'))
@@ -33,23 +56,23 @@ function init() {
     tab_moments.addEventListener('mouseout', () => moveUnderline())
     tab_post.addEventListener('mouseout', () => moveUnderline())
 
-    tab_home.addEventListener('click', function() {
+    tab_home.addEventListener('click', function () {
         queries.tab = 'home'
-        history.pushState(null ,null ,`/space/?${jsonToQuery(queries)}`)
+        history.pushState(null, null, `/space/?${jsonToQuery(queries)}`)
         tab_home.classList.add('now_tab')
         tab_moments.classList.remove('now_tab')
         tab_post.classList.remove('now_tab')
     })
-    tab_moments.addEventListener('click', function() {
+    tab_moments.addEventListener('click', function () {
         queries.tab = 'moments'
-        history.pushState(null ,null ,`/space/?${jsonToQuery(queries)}`)
+        history.pushState(null, null, `/space/?${jsonToQuery(queries)}`)
         tab_home.classList.remove('now_tab')
         tab_moments.classList.add('now_tab')
         tab_post.classList.remove('now_tab')
     })
-    tab_post.addEventListener('click', function() {
+    tab_post.addEventListener('click', function () {
         queries.tab = 'post'
-        history.pushState(null ,null ,`/space/?${jsonToQuery(queries)}`)
+        history.pushState(null, null, `/space/?${jsonToQuery(queries)}`)
         tab_home.classList.remove('now_tab')
         tab_moments.classList.remove('now_tab')
         tab_post.classList.add('now_tab')
